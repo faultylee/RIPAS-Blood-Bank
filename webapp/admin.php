3bacd2c2661d4ee2458475f27c0e0dd8a0120f72
Firdaus Zailani <mfirdaus@SurfacePro.local>
2014-01-12 16:28:49 +0800

    lots of stuff

--- webapp/admin.php
+++ webapp/admin.php
@@ -2,20 +2,51 @@
 
 require_once("DB.php");
 DB::setup("blood","root","");
-$blood_type=json_decode(file_get_contents("blood_types.json"));
+$temp_data=json_decode(file_get_contents("blood_types.json"));
 
-function get_compatible($o){
-	global $blood_type;
-	foreach($blood_type as $b){
-		if($b[0]==$o)
-			break;
+$status_level=array("N/A","very low","low","ok");
+
+$blood_type=array();
+foreach($temp_data as $b){
+	$blood_type[$b[0]]=array("give"=>explode(",",$b[1]),"recieve"=>explode(",",$b[2]));
+}
+
+$bank_id=1;
+
+if(@$_POST["action"]=="update_blood"){
+	foreach($blood_type as $type=>$data){
+		$status=@$_POST["status_".$type];
+		if($status)
+			DB::query("insert into bloods(bank_id,type,status) values(?,?,?)",array($bank_id,$type,$status));
 	}
-	print_r($b);
 }
 
 
-print_r(get_compatible("A+"));
 
 
 ?>
+<pre>
+<?php print_r($_POST); ?>
+</pre>
 
+
+<style>
+	label {width:40px;display:block;float:left;}
+</style>
+
+<h1>Update Levels</h1>
+<form method="post">
+<input type="hidden" name="action" value="update_blood"/>
+<?php foreach($blood_type as $type=>$data): ?>
+<label><?=$type;?></label>
+<select name="status_<?=$type;?>">
+<?php foreach($status_level as $i=>$n): ?>
+<option value="<?=$i;?>"><?=$n;?></option>
+<?php endforeach; ?>
+</select><br/>
+<?php endforeach; ?>
+<input type="submit"/>
+</form>
+<?php
+
+?>
