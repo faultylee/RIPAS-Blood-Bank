11248b4c1958ec941ce721cf990ec5b0ccdacd42
Firdaus Zailani <mfirdaus@SurfacePro.local>
2014-01-12 17:43:48 +0800

    fixed json.php

--- webapp/json.php
+++ webapp/json.php
@@ -5,7 +5,7 @@ require_once("header.php");
 $temp=get_latest(1); //
 $return=array();
 foreach($temp as $b){
-	$return[$b["type"]]=$status_level[$b["status"]];
+	$return[]=array("type"=>$b["type"],"status"=>$status_level[$b["status"]]);
 }
 
 echo json_encode($return);
