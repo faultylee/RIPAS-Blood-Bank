99c9e531ea2aac9bdc8ec6dfe4fd6b55c5f89bca
Firdaus Zailani <mfirdaus@SurfacePro.local>
2014-01-12 17:16:46 +0800

    header.php

--- webapp/admin.php
+++ webapp/admin.php
@@ -1,29 +1,7 @@
 <?php
-
-require_once("DB.php");
-require_once("config.php");
-DB::setup(DB_NAME, DB_USER, DB_PASS, DB_HOST);
-DB::debug();
-$temp_data=json_decode(file_get_contents("blood_types.json"));
-
-$status_level=array("N/A","very low","low","ok");
-
-$blood_type=array();
-foreach($temp_data as $b){
-	$blood_type[$b[0]]=array("give"=>explode(",",$b[1]),"recieve"=>explode(",",$b[2]));
-}
+require_once("header.php");
 
 $bank_id=1;
-
-function get_latest($bank_id){
-	$temp=DB::fetchAll("select * from (SELECT * FROM bloods where bank_id=? order by time desc) b  GROUP BY TYPE , bank_id;",$bank_id);
-	$return=array();
-	foreach($temp as $blood){
-		$return[$blood["type"]]=$blood;
-	}
-	return $return;
-}
-
 $latest=get_latest(1);
 if(@$_POST["action"]=="update_blood"){
 	foreach($blood_type as $type=>$data){
