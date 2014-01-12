80a5aaf663117ae68d32597a15ea61d8f21399e5
Firdaus Zailani <mfirdaus@SurfacePro.local>
2014-01-12 16:04:39 +0800

    basic admin

--- webapp/admin.php
+++ webapp/admin.php
@@ -1,3 +1,19 @@
 <?php
-print_r($_POST);
+
+require_once("DB.php");
+DB::setup("blood","root","");
+$blood_type=json_decode(file_get_contents("blood_types.json"));
+
+function get_compatible($o){
+	global $blood_type;
+	foreach($blood_type as $b){
+		if($b[0]==$o)
+			break $b;
+	}
+}
+
+print_r($blood_type);
+
+
 ?>
+
